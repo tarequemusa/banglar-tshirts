b873d6c68e7fd4e77bf17598b09d05ec441b7fab
tarequemusa <tareque.web@gmail.com>
2023-04-07 02:22:27 +0600

    2nd Commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>This is Home Page: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
